277fb2d4617567cced4df66d025fb10cd2483b66
Jethro Whaley <jethro.whaley@gmail.com>
2018-11-29 21:58:10 +0000

    Somre refactor post pairing - rspec tests written more clearly

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,13 +1,13 @@
 class Dockingstation
 
-  attr_reader :bike 
+  attr_reader :bike
 
   def initialize
-    @capacity = Array.new
+    @capacity = []
   end
 
   def release_bike
-    fail "No bikes available" unless @capacity.length > 0 
+    fail "No bikes available" unless @capacity.length > 0
     @capacity.pop
   end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,21 +2,19 @@ require 'docking_station'
 
 describe Dockingstation do
 
-  it { is_expected.to respond_to :release_bike }
-
-  # check step 11
-  # it 'gets a bike and check if working' do
-  #   bike = Dockingtation.new.release_bike
-  #   expect(bike.working?).to eq true
-  # end
-
-  it {is_expected.to respond_to(:dock).with(1).argument}
-
-  it "raises an error when no bikes are available" do
-    expect { Dockingstation.new.release_bike }.to raise_error(RuntimeError)
+  it "Docking station respods to #release_bike method" do
+    expect(subject).to respond_to :release_bike
   end
 
-  describe '#dock' do 
+  it "Docking station responds to #dock with 1 argument" do
+    expect(subject).to respond_to(:dock).with(1).argument
+  end
+
+  it "raises an error when no bikes are available" do
+    expect { subject.release_bike }.to raise_error 'No bikes available'
+  end
+
+  describe '#dock' do
     it "raises an error when docking station is full" do
       20.times { subject.dock(Bike.new) }
       expect { subject.dock(Bike.new) }.to raise_error 'Docking Station is full'
